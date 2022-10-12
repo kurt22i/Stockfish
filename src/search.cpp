538e44e68d3d87fb9a52195337be44bdec04ad02
kurt22i <streamratings2@gmail.com>
2022-10-12 01:14:58 -0400

    3607363

--- src/search.cpp
+++ src/search.cpp
@@ -1171,7 +1171,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (quietCount > 10)
+          if (quietCount > 3)
               r++;
 
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
