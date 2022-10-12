78344ee67781f64fa82ded8c7e22a5f93cddc443
kurt22i <streamratings2@gmail.com>
2022-10-12 09:35:15 -0400

    quadratic 50mr

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1076,8 +1076,8 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
       v = (nnue * scale + optimism * (scale - 754)) / 1024;
   }
 
-  // Damp down the evaluation linearly when shuffling
-  v = v * (195 - pos.rule50_count()) / 211;
+  // Damp down the evaluation when shuffling
+  v = v * (pos.rule50_count()*pos.rule50_count()*6 - 1073*pos.rule50_count() + 92420) / 100000;
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
