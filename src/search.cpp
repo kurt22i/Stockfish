5fb6cc743d75e921b1b03b47c344cf291004f7ee
kurt22i <streamratings2@gmail.com>
2022-10-12 15:15:07 -0400

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1171,6 +1171,9 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
+          if (pos.rule50_count() > 12 && !improving && depth > 12)
+              r++;
+
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
