a74c41a783142c57baecb083502a6607f4e8c7e4
kurt22i <streamratings2@gmail.com>
2022-10-13 16:55:59 -0400

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -74,9 +74,9 @@ namespace {
     return (r + 1642 - int(delta) * 1024 / int(rootDelta)) / 1024 + (!i && r > 916);
   }
 
-  constexpr int futility_move_count(bool improving, Depth depth) {
+  constexpr int futility_move_count(bool improving, Depth depth, int r50c) {
     return improving ? (3 + depth * depth)
-                     : (3 + depth * depth) / 2;
+                     : (3 + depth * depth - r50c) / 2;
   }
 
   // History and stats update bonus, based on depth
@@ -993,7 +993,7 @@ moves_loop: // When in check, search starts here
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~7 Elo)
-          moveCountPruning = moveCount >= futility_move_count(improving, depth);
+          moveCountPruning = moveCount >= futility_move_count(improving, depth, pos.rule50_count());
 
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, delta, thisThread->rootDelta), 0);
