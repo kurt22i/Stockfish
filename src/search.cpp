1880fb8476ff5e0b871f0c2562f10af97181d64f
kurt22i <streamratings2@gmail.com>
2022-10-14 10:59:54 -0400

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1162,7 +1162,7 @@ moves_loop: // When in check, search starts here
           if (singularQuietLMR)
               r--;
 
-          // Dicrease reduction if we move a threatened piece (~1 Elo)
+          // Decrease reduction if we move a threatened piece (~1 Elo)
           if (   depth > 9
               && (mp.threatenedPieces & from_sq(move)))
               r--;
@@ -1170,6 +1170,10 @@ moves_loop: // When in check, search starts here
           // Increase reduction if next ply has a lot of fail high
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
+
+          // Increase reduction if position seems stagnant
+          if (pos.rule50_count() > 12 && !improving && depth > 12)
+              r += 3;
 
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
