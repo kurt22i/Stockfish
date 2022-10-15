06b0b1fa41d0e7ae10b5a6cdb68e3b1e0400b085
kurt22i <streamratings2@gmail.com>
2022-10-15 12:09:11 -0400

    Bench: 4278727

--- src/search.cpp
+++ src/search.cpp
@@ -1143,7 +1143,7 @@ moves_loop: // When in check, search starts here
               r -= 2;
 
           // Decrease reduction if opponent's move count is high (~1 Elo)
-          if ((ss-1)->moveCount > 7)
+          if ((ss-1)->moveCount > 7 && pos.rule50_count() <= 12)
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
