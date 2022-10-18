6aad3b3088fba97ea46a91b5a2eb9504f6ce2003
kurt22i <streamratings2@gmail.com>
2022-10-18 08:31:55 -0400

    Bench: 4425376

--- src/search.cpp
+++ src/search.cpp
@@ -1092,6 +1092,14 @@ moves_loop: // When in check, search starts here
                   extension = -1;
           }
 
+          else if (   capture
+                   && !improving
+                   && depth < 9
+                   && abs(ss->staticEval) > 82
+                   && pos.count<PAWN>() >= 10
+                   && complexity > 625)
+              extension = 1;
+
           // Check extensions (~1 Elo)
           else if (   givesCheck
                    && depth > 9
