b3e05bf31a21082268fb1c37f1d1dd1247b056a4
kurt22i <streamratings2@gmail.com>
2022-10-18 21:49:39 -0400

    Bench: 4356604

--- src/search.cpp
+++ src/search.cpp
@@ -1005,6 +1005,7 @@ moves_loop: // When in check, search starts here
                   && !PvNode
                   && lmrDepth < 7
                   && !ss->inCheck
+                  && pos.rule50_count() <= 20
                   && ss->staticEval + 180 + 201 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))]
                    + captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] / 6 < alpha)
                   continue;
