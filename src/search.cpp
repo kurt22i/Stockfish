d2d1469edf5399bc3f0b18cce520937cb328e9ee
kurt22i <streamratings2@gmail.com>
2022-10-19 23:47:45 -0400

    Bench: 4336243

--- src/search.cpp
+++ src/search.cpp
@@ -897,7 +897,7 @@ namespace {
     // Use qsearch if depth is equal or below zero (~4 Elo)
     if (    PvNode
         && !ttMove)
-        depth -= 3;
+        depth -= 3 - (depth < pos.rule50_count());
 
     if (depth <= 0)
         return qsearch<PV>(pos, ss, alpha, beta);
