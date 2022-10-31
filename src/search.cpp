90b1e9708e4c9afb46b2ad54f3a1ad4a7eb4bbca
kurt22i <streamratings2@gmail.com>
2022-10-30 22:50:45 -0400

    bench 4532951

--- src/search.cpp
+++ src/search.cpp
@@ -1547,6 +1547,7 @@ moves_loop: // When in check, search starts here
 
       // Continuation history based pruning (~2 Elo)
       if (   !capture
+          && !ss->inCheck
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY
           && (*contHist[0])[pos.moved_piece(move)][to_sq(move)] < 0
           && (*contHist[1])[pos.moved_piece(move)][to_sq(move)] < 0)
