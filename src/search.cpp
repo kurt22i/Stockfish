7ff09cfa3cf51867c66ff4c60c549074a3cbfbe2
kurt22i <streamratings2@gmail.com>
2022-11-01 11:11:55 -0400

    bench 4996390

--- src/search.cpp
+++ src/search.cpp
@@ -1704,7 +1704,7 @@ moves_loop: // When in check, search starts here
     {
         moved_piece = pos.moved_piece(capturesSearched[i]);
         captured = type_of(pos.piece_on(to_sq(capturesSearched[i])));
-        captureHistory[moved_piece][to_sq(capturesSearched[i])][captured] << -bonus1;
+        captureHistory[moved_piece][to_sq(capturesSearched[i])][captured] << -bonus1 / (1 + (to_sq(capturesSearched[i]) == to_sq(bestMove)));
     }
   }
 
